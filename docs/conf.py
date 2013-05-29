5f8fd35c201be4b6540419bf13ac536efd0b7699
Stefan Richter <stefan@02strich.de>
2013-05-29 11:33:57 +0200

    change doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,8 @@ html_theme = 'default'
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
+html_style = "/default.css"
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None

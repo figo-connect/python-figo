29d996217171674e5f019b3f34d5adc51ff60245
achimsen <jp@figo.io>
2016-07-27 17:27:58 +0200

    added newest fingerprint

--- figo/figo.py
+++ figo/figo.py
@@ -28,7 +28,7 @@ import json
 
 logger = logging.getLogger(__name__)
 
-VALID_FINGERPRINT = ("38:AE:4A:32:6F:16:EA:15:81:33:8B:B0:D8:E4:A6:35:E7:27:F1:07")
+VALID_FINGERPRINT = ("38:AE:4A:32:6F:16:EA:15:81:33:8B:B0:D8:E4:A6:35:E7:27:F1:07", "DB:E2:E9:15:8F:C9:90:30:84:FE:36:CA:A6:11:38:D8:5A:20:5D:93")
 
 ERROR_MESSAGES = {
     400: {'message': "bad request", 'description': "Bad request"},

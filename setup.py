2afcb8cb537d5431e74d35723cd2c1ae09ebd588
fricklerhandwerk <valentin@fricklerhandwerk.de>
2017-06-19 14:47:18 +0200

    use pbr

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python
+
 from setuptools import setup
 
-setup(name='python-figo',
-      version='1.6.1',
-      description='Library to easily use the API of https://figo.io',
-      author='figo GmbH',
-      author_email='devs@figo.io',
-      url='https://www.figo.io',
-      packages=['figo'],
-      license='BSD License',
-      classifiers=[
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Topic :: Office/Business :: Financial',
-          'Topic :: Internet'
-      ])
+setup(setup_requires=['pbr'], pbr=True)

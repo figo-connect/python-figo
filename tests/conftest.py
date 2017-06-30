501f0f003e71e6ab5ed2f32902e8ce46aa7e1272
Berend Kapelle <berend.kapelle@gmail.com>
2017-06-30 13:08:43 +0200

    changed fingerprint in test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,8 @@ DEMO_CREDENTIALS = {
     'client_id': 'C-9rtYgOP3mjHhw0qu6Tx9fgk9JfZGmbMqn-rnDZnZwI',
     'client_secret': 'Sv9-vNfocFiTe_NoMRkvNLe_jRRFeESHo8A0Uhyp7e28',
     'api_endpoint': 'https://api.figo.me',
-    'ssl_fingerprint': ('07:0F:14:AE:B9:4A:FB:3D:F8:00:E8:2B:69:A8:51:5C:'
-                        'EE:D2:F5:B1:BA:89:7B:EF:64:32:45:8F:61:CF:9E:33'),
+    'ssl_fingerprint': ('79:B2:A2:93:00:85:3B:06:92:B1:B5:F2:24:79:48:58:3A:A5:22:0F:C5:CD:E9:49:9A:C8:45:1E:DB:E0:DA'
+                        ':50'),
 }
 
 CREDENTIALS = {

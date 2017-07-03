c38b54fefe86b5881baf2db634735dcd3680c9e6
fricklerhandwerk <valentin@fricklerhandwerk.de>
2017-07-03 13:30:48 +0200

    flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
-import uuid
-from logging import basicConfig
 import os
-
 import pytest
+import uuid
 
-from figo.figo import FigoConnection, FigoSession
+from logging import basicConfig
+
+from figo.figo import FigoConnection
+from figo.figo import FigoSession
 
 basicConfig(level='DEBUG')
 
@@ -12,8 +13,8 @@ DEMO_CREDENTIALS = {
     'client_id': 'C-9rtYgOP3mjHhw0qu6Tx9fgk9JfZGmbMqn-rnDZnZwI',
     'client_secret': 'Sv9-vNfocFiTe_NoMRkvNLe_jRRFeESHo8A0Uhyp7e28',
     'api_endpoint': 'https://api.figo.me',
-    'ssl_fingerprint': ('79:B2:A2:93:00:85:3B:06:92:B1:B5:F2:24:79:48:58:3A:A5:22:0F:C5:CD:E9:49:9A:C8:45:1E:DB:E0:DA'
-                        ':50'),
+    'ssl_fingerprint': ('79:B2:A2:93:00:85:3B:06:92:B1:B5:F2:24:79:48:58:'
+                        '3A:A5:22:0F:C5:CD:E9:49:9A:C8:45:1E:DB:E0:DA:50'),
 }
 
 CREDENTIALS = {
@@ -25,8 +26,8 @@ CREDENTIALS = {
 
 PASSWORD = 'some_words'
 
-DEMO_TOKEN = ('ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexT'
-              'o22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ')
+DEMO_TOKEN = ('ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo'
+              '22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ')
 
 
 def is_demo(credentials):

8cd303c5dfba99eef8d0edfbe70a0fe464927940
Berend Kapelle <bk@figo.io>
2018-06-14 17:43:43 +0200

    fix credentials.py

--- figo/credentials.py
+++ figo/credentials.py
@@ -8,7 +8,7 @@ DEMO_CREDENTIALS = {
     'ssl_fingerprints': ('79:B2:A2:93:00:85:3B:06:92:B1:B5:F2:24:79:48:58:'
                          '3A:A5:22:0F:C5:CD:E9:49:9A:C8:45:1E:DB:E0:DA:50',
                          'CD:F3:D3:26:27:89:91:B9:CD:AE:4B:10:6C:96:81:B7:'
-                         'EB:B3:38:10:C4:72:37:6A:4D:9C:84:B7:B3:DC:D6:8D),
+                         'EB:B3:38:10:C4:72:37:6A:4D:9C:84:B7:B3:DC:D6:8D'),
 }
 
 DEMO_TOKEN = ('ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo'

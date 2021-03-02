1ff6375643ecff676c977b1185b7e6597853bfdc
Daniel Konwisorz <dk@ownly.de>
2021-03-02 15:54:24 +0100

    WDFO-81 - fix notifications test cases

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -1,56 +1,71 @@
 import pytest
-import os
 
-from figo import FigoConnection
 from figo import FigoSession
 from figo.models import Notification
 
-API_ENDPOINT = os.getenv("FIGO_API_ENDPOINT")
-CLIENT_ID = os.getenv("FIGO_CLIENT_ID")
-CLIENT_SECRET = os.getenv("FIGO_CLIENT_SECRET")
-connection  = FigoConnection(CLIENT_ID, CLIENT_SECRET, "https://127.0.0.1/", api_endpoint=API_ENDPOINT)
+from .conftest import PASSWORD
 
-def pytest_namespace():
-  return {'session': '', 'token': '', 'access_id': '', 'sync_id': '', 'challenge_id': ''}
 
-#To call only once
-def test_add_user():
-  response = connection.add_user("John Doe", "john.doe@example.com", "password")
-  assert response == {}
+def pytest_configure():
+    pytest.session = None
+    pytest.token = None
+    pytest.access_id = None
+    pytest.sync_id = None
+    pytest.challenge_id = None
+    pytest.account_id = None
+    pytest.new_user_id = None
 
-def test_create_token_and_session():
-  token = connection.credential_login("john.doe@example.com", "password")
-  pytest.token = token["access_token"]
-  assert pytest.token
-  pytest.session = FigoSession(token["access_token"])
-  assert pytest.session.user.full_name == "John Doe"
 
-def test_add_notification(access_token):
+# To call only once
+def test_add_user(figo_connection, new_user_id):
+    response = figo_connection.add_user("Jimmy", new_user_id, PASSWORD)
+    pytest.new_user_id = new_user_id
+    assert response == {}
+
+
+def test_create_token_and_session(figo_connection):
+    token = figo_connection.credential_login(pytest.new_user_id, PASSWORD)
+    pytest.token = token["access_token"]
+    assert pytest.token
+    pytest.session = FigoSession(token["access_token"])
+    assert pytest.session.user.full_name == "Jimmy"
+
+
+def test_add_notification():
     notification = Notification(pytest.session)
     notification.notify_uri = "https://api.figo.me/callback"
     notification.observe_key = "/rest/transactions"
     notification.state = "4HgwtQP0jsjdz79h"
     response = pytest.session.add_notification(notification)
     pytest.notification = response
-    assert response.notification_id != None
+    assert response.notification_id is not None
 
-def test_notifications(access_token):
+
+def test_notifications():
     response = pytest.session.notifications
-    assert response != None
+    assert response is not None
 
-def test_modify_notification(access_token):
-    pytest.notification.state="ZZZ"
+
+def test_modify_notification():
+    pytest.notification.state = "ZZZ"
     response = pytest.session.modify_notification(pytest.notification)
-    assert response.state != None
+    assert response.state is not None
 
-def test_get_notification(access_token):
-    response = pytest.session.get_notification(pytest.notification.notification_id)
-    assert response.state != None
 
-def test_remove_notification(access_token):
-    response = pytest.session.remove_notification(pytest.notification.notification_id)
-    assert response == None
+def test_get_notification():
+    response = pytest.session.get_notification(
+        pytest.notification.notification_id
+    )
+    assert response.state is not None
+
+
+def test_remove_notification():
+    response = pytest.session.remove_notification(
+        pytest.notification.notification_id
+    )
+    assert response is None
+
 
 def test_remove_user():
-  response = pytest.session.remove_user()
-  assert response == {}
+    response = pytest.session.remove_user()
+    assert response == {}

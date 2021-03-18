01b00bdaffa24ef07aa38dea30776bce906d44b6
Daniel Konwisorz <dk@ownly.de>
2021-03-18 12:27:48 +0100

    WDFO-81 - update stage version on integration testing

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -41,7 +41,7 @@ def test_add_user(figo_connection, new_user_id):
 
 def test_get_version(figo_connection):
     response = figo_connection.get_version()
-    assert response == {"environment": "staging", "version": "20.18.6"}
+    assert response == {"environment": "staging", "version": "21.1.0"}
 
 
 def test_create_token_and_session(figo_connection):
